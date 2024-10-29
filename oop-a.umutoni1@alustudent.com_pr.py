2706c6d64759739f6e68c6d66948b2550e155d3e
angeumutoni <a.umutoni1@alustudent.com>
2024-10-29 09:05:26 +0200

    Adding class attributes

--- oop-a.umutoni1@alustudent.com_pr.py
+++ oop-a.umutoni1@alustudent.com_pr.py
@@ -1,2 +1,8 @@
 class Patient:
- 
+  def __init__(self, id, name, age, gender, admission_date, condition):
+    self.id = id
+    self.name = name
+    self.age= age
+    self.gender = gender
+    self.admission_date = admission_date
+    self.condition = condition
