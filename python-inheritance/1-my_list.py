6af6bd5384770d5515a9cfd9672b860c24ee425d
angeumutoni <a.umutoni1@alustudent.com>
2024-11-08 22:07:45 -0800

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-''' Module: 1-my_list
-'''
-
-
 class MyList(list):
-    ''' Represents a MyList
-    '''
+    ''' creating a public instance method '''
 
     def print_sorted(self):
-        '''
-        prints the list, but sorted
-        '''
+        ''' printing the sorted list '''
         print(sorted(self))
