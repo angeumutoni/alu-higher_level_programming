6263a673f248c016549e70206d6e69b5940070af
angeumutoni <a.umutoni1@alustudent.com>
2024-11-09 00:49:33 -0800

    Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+''' module: 2-is_same_class
+'''
+
 
 def is_kind_of_class(obj, a_class):
-    '''we are using the is_kind_of_class function, obj as object'''
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
     return isinstance(obj, a_class)
