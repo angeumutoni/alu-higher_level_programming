11f5cecfa58e3abfbc7b1e0f8965ed348c145ede
angeumutoni <a.umutoni1@alustudent.com>
2024-11-16 00:52:34 -0800

    To JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' For this task, we are creating a function that returns the JSON representation of an object (string)
+''' function that returns the JSON representation of an object (string)
 '''
 
 import json
