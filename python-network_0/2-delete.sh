b0d3fcf736a7df4d639af5d717b3dccd320c903c
angeumutoni <a.umutoni1@alustudent.com>
2024-11-20 23:18:55 -0800

    cURL Method

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL and displays all HTTP methods the server will accept
+# A script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sX DELETE "$1"
